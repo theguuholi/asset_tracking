1fd3dad589d82f013bfebfb63b2d16d8712ad4d2
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-16 16:55:30 -0300

    add a purchase

--- lib/asset_tracker.ex
+++ lib/asset_tracker.ex
@@ -1,6 +1,20 @@
 defmodule AssetTracker do
-  defstruct value: nil
+  defstruct symbol: nil,
+            settle_date: Date.utc_today(),
+            quantity: nil,
+            unit_price: nil
+
   def new do
     %__MODULE__{}
+  end
+
+  def add_purchase(asset_tracker, symbol, settle_date, quantity, unit_price) do
+    %{
+      asset_tracker
+      | symbol: symbol,
+        settle_date: settle_date,
+        quantity: quantity,
+        unit_price: unit_price
+    }
   end
 end
--- test/asset_tracker_test.exs
+++ test/asset_tracker_test.exs
@@ -1,7 +1,19 @@
 defmodule AssetTrackerTest do
   use ExUnit.Case
+  import AssetTracker
 
-  test "greets the world" do
-    assert AssetTracker.new() == %AssetTracker{}
+  test "new/0" do
+    assert new() == %AssetTracker{}
+  end
+
+  test "add_purchase/5" do
+    today = Date.utc_today()
+
+    assert add_purchase(new(), "STN", today, 10, 3) == %AssetTracker{
+             symbol: "STN",
+             settle_date: today,
+             quantity: 10,
+             unit_price: 3
+           }
   end
 end
