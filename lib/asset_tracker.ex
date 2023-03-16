ed35bed91ab1597f509cdcd49f1ee78987f682f9
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-16 19:27:43 -0300

    fix tracker

--- lib/asset_tracker.ex
+++ lib/asset_tracker.ex
@@ -1,20 +1,44 @@
 defmodule AssetTracker do
-  defstruct symbol: nil,
-            settle_date: Date.utc_today(),
-            quantity: nil,
-            unit_price: nil
+  # defstruct symbol: nil,
+  #           settle_date: Date.utc_today(),
+  #           quantity: nil,
+  #           unit_price: nil
+  defstruct assets: %{}
 
   def new do
     %__MODULE__{}
   end
 
   def add_purchase(asset_tracker, symbol, settle_date, quantity, unit_price) do
+    asset_info = %{
+      settle_date: settle_date,
+      quantity: quantity,
+      unit_price: unit_price
+    }
+
     %{
       asset_tracker
-      | symbol: symbol,
-        settle_date: settle_date,
-        quantity: quantity,
+      | assets:
+          Map.update(asset_tracker.assets, symbol, [asset_info], fn assets_info ->
+            assets_info ++ [asset_info]
+          end)
+    }
+  end
+
+  def add_sale(asset_tracker, _symbol, sell_date, quantity, unit_price) do
+    # symbol, seems not to make sense
+    # I did not understand the advantage of using gain and loss
+    gain = quantity * (unit_price - asset_tracker.unit_price)
+
+    quantity_updated = asset_tracker.quantity - quantity
+
+    asset_tracker = %{
+      asset_tracker
+      | settle_date: sell_date,
+        quantity: quantity_updated,
         unit_price: unit_price
     }
+
+    {asset_tracker, %{gain: gain, loss: 0}}
   end
 end
--- test/asset_tracker_test.exs
+++ test/asset_tracker_test.exs
@@ -3,17 +3,72 @@ defmodule AssetTrackerTest do
   import AssetTracker
 
   test "new/0" do
-    assert new() == %AssetTracker{}
+    assert new() == %AssetTracker{assets: %{}}
   end
 
   test "add_purchase/5" do
     today = Date.utc_today()
 
     assert add_purchase(new(), "STN", today, 10, 3) == %AssetTracker{
-             symbol: "STN",
-             settle_date: today,
-             quantity: 10,
-             unit_price: 3
+             assets: %{
+               "STN" => [
+                 %{
+                   settle_date: today,
+                   quantity: 10,
+                   unit_price: 3
+                 }
+               ]
+             }
            }
+  end
+
+  test "add_purchase/5 add twice same assets" do
+    today = Date.utc_today()
+
+    assert new()
+           |> add_purchase("STN", today, 10, 3)
+           |> add_purchase("STN", today, 15, 6) ==
+             %AssetTracker{
+               assets: %{
+                 "STN" => [
+                   %{
+                     settle_date: today,
+                     quantity: 10,
+                     unit_price: 3
+                   },
+                   %{
+                     settle_date: today,
+                     quantity: 15,
+                     unit_price: 6
+                   }
+                 ]
+               }
+             }
+  end
+
+  test "add_purchase/5 with different symbol" do
+    today = Date.utc_today()
+
+    assert new()
+           |> add_purchase("STN", today, 10, 3)
+           |> add_purchase("Apl", today, 13, 4) ==
+             %AssetTracker{
+               assets: %{
+                 "STN" => [
+                   %{
+                     settle_date: today,
+                     quantity: 10,
+                     unit_price: 3
+                   }
+                 ],
+                 "Apl" => [
+                   %{
+                     settle_date: today,
+                     quantity: 13,
+                     unit_price: 4
+                   }
+                 ]
+               }
+             }
   end
 end
