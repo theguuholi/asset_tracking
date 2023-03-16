1fcbe997331b7cf3c7c386a173f999c0e5866b24
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-16 16:44:27 -0300

    start

---
+++ lib/asset_tracking.ex
@@ -0,0 +1,18 @@
+defmodule AssetTracking do
+  @moduledoc """
+  Documentation for `AssetTracking`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AssetTracking.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/asset_tracking_test.exs
@@ -0,0 +1,8 @@
+defmodule AssetTrackingTest do
+  use ExUnit.Case
+  doctest AssetTracking
+
+  test "greets the world" do
+    assert AssetTracking.hello() == :world
+  end
+end
