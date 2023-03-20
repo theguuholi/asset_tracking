d186e191a0feecd123c43d17f00d0412b46cbefa
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-20 06:11:09 -0300

    refactor test

--- test/asset_tracker_test.exs
+++ test/asset_tracker_test.exs
@@ -6,69 +6,72 @@ defmodule AssetTrackerTest do
     assert new() == %AssetTracker{assets: %{}}
   end
 
-  test "add_purchase/5" do
-    today = DateTime.utc_now()
+  describe "add purchase" do
+    test "given a new asset to add to wallet, when add return the assets list" do
+      today = DateTime.utc_now()
 
-    assert add_purchase(new(), "STN", today, 10, 3) == %AssetTracker{
-             assets: %{
-               "STN" => [
-                 %{
-                   settle_date: today,
-                   quantity: 10,
-                   unit_price: 3
+      assert add_purchase(new(), "STN", today, 10, 3) == %AssetTracker{
+               assets: %{
+                 "STN" => [
+                   %{
+                     settle_date: today,
+                     quantity: 10,
+                     unit_price: 3
+                   }
+                 ]
+               }
+             }
+    end
+
+    test "given an asset that user bought twice, when add the asset, then return using a list when they bought the asset" do
+      today = DateTime.utc_now()
+      tomorrow = today |> DateTime.add(1, :day)
+
+      assert new()
+             |> add_purchase("STN", today, 10, 3)
+             |> add_purchase("STN", tomorrow, 15, 6) ==
+               %AssetTracker{
+                 assets: %{
+                   "STN" => [
+                     %{
+                       settle_date: today,
+                       quantity: 10,
+                       unit_price: 3
+                     },
+                     %{
+                       settle_date: tomorrow,
+                       quantity: 15,
+                       unit_price: 6
+                     }
+                   ]
                  }
-               ]
-             }
-           }
-  end
-
-  test "add_purchase/5 add twice same assets" do
-    today = DateTime.utc_now()
-
-    assert new()
-           |> add_purchase("STN", today, 10, 3)
-           |> add_purchase("STN", today, 15, 6) ==
-             %AssetTracker{
-               assets: %{
-                 "STN" => [
-                   %{
-                     settle_date: today,
-                     quantity: 10,
-                     unit_price: 3
-                   },
-                   %{
-                     settle_date: today,
-                     quantity: 15,
-                     unit_price: 6
-                   }
-                 ]
                }
-             }
-  end
+    end
 
-  test "add_purchase/5 with different symbol" do
-    today = DateTime.utc_now()
+    test "given an user that has assets, when bought a new one, then return the new asset" do
+      today = DateTime.utc_now()
 
-    assert new()
-           |> add_purchase("STN", today, 10, 3)
-           |> add_purchase("Apl", today, 13, 4) ==
-             %AssetTracker{
-               assets: %{
-                 "STN" => [
-                   %{
-                     settle_date: today,
-                     quantity: 10,
-                     unit_price: 3
-                   }
-                 ],
-                 "Apl" => [
-                   %{
-                     settle_date: today,
-                     quantity: 13,
-                     unit_price: 4
-                   }
-                 ]
+      assert new()
+             |> add_purchase("STN", today, 10, 3)
+             |> add_purchase("Apl", today, 13, 4) ==
+               %AssetTracker{
+                 assets: %{
+                   "STN" => [
+                     %{
+                       settle_date: today,
+                       quantity: 10,
+                       unit_price: 3
+                     }
+                   ],
+                   "Apl" => [
+                     %{
+                       settle_date: today,
+                       quantity: 13,
+                       unit_price: 4
+                     }
+                   ]
+                 }
                }
-             }
+    end
   end
 end
